84a82af8ba77238541242a71edeaa86f9d772152
Sameer <sameer.mahmud1@hotmail.com>
2021-08-21 17:19:00 +1000

    needs work

--- src/components/CovidChart.js
+++ src/components/CovidChart.js
@@ -1,40 +1,31 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
 import {Line} from 'react-chartjs-2'
+import GetData from './GetData';
 
-const API_URL = "https://covid-api.mmediagroup.fr/v1/history?country=Australia&status=confirmed";
 
-const DAILY_CASES = "https://covid-api.mmediagroup.fr/v1/cases?country=Australia";
 
 const CovidChart = () => {
-    const [data, setData] = useState(null);
-    const [dailyData, setDailyData] = useState(null);
-    //Updates data and places into data array using axios library
-    useEffect(() => {
-        axios.get(API_URL).then(res => setData(res.data));
-        axios.get(DAILY_CASES).then(res => setDailyData(res.data));
+    const[covidData, setData] = useState(null)
+    var newData = GetData();
+    useEffect(()=> {
+        setData(newData)
     }, []);
-
-
-
-    //Ensures data is not null, parses the JSON into NSW data and VIC data
-    if (data && dailyData) {
-    var labels = Object.keys(data["All"].dates).reverse();
-        //NSW DATA 
-    labels.push("21/08/21");
-      var nswCases = Object.values(data["New South Wales"].dates).reverse();
-      nswCases.push(dailyData["New South Wales"].confirmed)
-      nswCases = nswCases.map((v,i,a) => v- (a[i-1] || 0));
-      //VIC DATA
-      var vicCases = Object.values(data["Victoria"].dates).reverse();
-      vicCases.push(dailyData["Victoria"].confirmed)
-      vicCases = vicCases.map((v,i,a) => v- (a[i-1] || 0));
+    if(covidData!=null){
+        var nswCases = covidData.nswCases;
+        var vicCases = covidData.vicCases
+        var labels = covidData.labels;
     }
 
-    
+    function update(){
+        nswCases = nswCases && nswCases.slice(-7);
+        vicCases = vicCases && vicCases.slice(-7);
+        labels = labels && labels.slice(-7);
+        setData({nswCases, vicCases, labels})
+    }
 
-    return (
-      <div>
+    return ( 
+       <div>
+          <button>1W</button>
         <Line
           data={{
             labels: labels,
