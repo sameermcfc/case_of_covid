4bcdd4b3ae43f003839d99077b71fd8d4fd0b99f
imranfire3307 <imranrah01@gmail.com>
2021-08-21 18:08:07 +1000

    graph functions still incomplete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import logo from './logo.svg';
-
 import CovidChart from './components/CovidChart';
 import './App.css';
 
 function App() {
-
-
-  return (
-    <div className="App">
-      <CovidChart/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <CovidChart/>
+        </div>
+    );
 }
 
 export default App;
--- src/components/CovidChart.js
+++ src/components/CovidChart.js
@@ -17,29 +17,45 @@ const CovidChart = () => {
     }
 
     function update(){
-        nswCases = nswCases && nswCases.slice(-7);
-        vicCases = vicCases && vicCases.slice(-7);
-        labels = labels && labels.slice(-7);
+        nswCases = nswCases.slice(-7);
+        vicCases = vicCases.slice(-7);
+        labels = labels.slice(-7);
+        setData({nswCases, vicCases, labels})
+    }
+
+    function update30(){
+        nswCases = nswCases.slice(-30);
+        vicCases = vicCases.slice(-30);
+        labels = labels.slice(-30);
+        setData({nswCases, vicCases, labels})
+    }
+
+    function update90(){
+        nswCases = nswCases && nswCases.slice(-90);
+        vicCases = vicCases && vicCases.slice(-90);
+        labels = labels && labels.slice(-90);
         setData({nswCases, vicCases, labels})
     }
 
     return ( 
        <div>
-          <button>1W</button>
+           <button onClick={update}>1W</button>
+           <button onClick={update30}>1M</button>
+           <button onClick={update90}>3M</button>
         <Line
           data={{
             labels: labels,
             datasets: [
               {
                 label: "NSW",
-                data: nswCases && nswCases,
+                data: nswCases,
                 fill: false,
                 borderColor: "#36a2eb",
                 tension: 0.1,
               },
               {
                 label: "Vic",
-                data: vicCases && vicCases,
+                data: vicCases,
                 fill: false,
                 borderColor: "#ff6384",
                 tension: 0.1,
