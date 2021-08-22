ca7f867012ccd4727ce525da2a9b55fd0d381d93
Sameer <sameer.mahmud1@hotmail.com>
2021-08-22 15:05:23 +1000

    working code for different frames

--- src/components/CovidChart.js
+++ src/components/CovidChart.js
@@ -1,61 +1,65 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import {Line} from 'react-chartjs-2'
-import GetData from './GetData';
 
+const API_URL =
+  "https://covid-api.mmediagroup.fr/v1/history?country=Australia&status=confirmed";
 
 
 const CovidChart = () => {
-    const[covidData, setData] = useState(null)
-    var newData = GetData();
-    useEffect(()=> {
-        setData(newData)
-    }, []);
-    if(covidData!=null){
-        var nswCases = covidData.nswCases;
-        var vicCases = covidData.vicCases
-        var labels = covidData.labels;
+  const[covidData, setData] = useState("");
+  
+  
+  useEffect(()=> {
+    if(!covidData){
+      getData();
+    }
+  }, []);
+
+  const getData = async (num) => {
+    let response = await axios.get(API_URL);
+    let data = await response.data;
+    
+    var labels = data && Object.keys(data["All"].dates).reverse();
+  //NSW DATA
+    var nswCases = data && Object.values(data["New South Wales"].dates).reverse();
+    nswCases = nswCases && nswCases.map((v, i, a) => v - (a[i - 1] || 0));
+  
+  //VIC DATA
+    var vicCases = data && Object.values(data["Victoria"].dates).reverse();
+    vicCases = vicCases && vicCases.map((v, i, a) => v - (a[i - 1] || 0));
+
+    if(num){
+      nswCases = nswCases.slice(num);
+      vicCases = vicCases.slice(num);
+      labels = labels.slice(num);
+      console.log(nswCases, vicCases, labels)
     }
 
-    function update(){
-        nswCases = nswCases.slice(-7);
-        vicCases = vicCases.slice(-7);
-        labels = labels.slice(-7);
-        setData({nswCases, vicCases, labels})
-    }
-
-    function update30(){
-        nswCases = nswCases.slice(-30);
-        vicCases = vicCases.slice(-30);
-        labels = labels.slice(-30);
-        setData({nswCases, vicCases, labels})
-    }
-
-    function update90(){
-        nswCases = nswCases && nswCases.slice(-90);
-        vicCases = vicCases && vicCases.slice(-90);
-        labels = labels && labels.slice(-90);
-        setData({nswCases, vicCases, labels})
-    }
+    setData({labels, nswCases, vicCases});
+  }
 
     return ( 
        <div>
-           <button onClick={update}>1W</button>
-           <button onClick={update30}>1M</button>
-           <button onClick={update90}>3M</button>
+           <button onClick = { () => {getData(-7)}}>1W</button>
+           <button onClick = { () => {getData(-30)}}>1M</button>
+           <button onClick = { () => {getData(-90)}}>3M</button>
+           <button onClick = { () => {getData(-180)}}>6M</button>
+           <button onClick = { () => {getData(-360)}}>12M</button>
         <Line
           data={{
-            labels: labels,
+            labels: covidData.labels,
             datasets: [
               {
                 label: "NSW",
-                data: nswCases,
+                data: covidData.nswCases,
                 fill: false,
                 borderColor: "#36a2eb",
                 tension: 0.1,
               },
               {
                 label: "Vic",
-                data: vicCases,
+                data: covidData.vicCases,
                 fill: false,
                 borderColor: "#ff6384",
                 tension: 0.1,
